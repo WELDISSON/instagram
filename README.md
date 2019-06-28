7ba443b87ae625f034c205cad4abb47823ad228f
Weldisson Araujo <weldisson.a.silva@accenture.com>
2019-06-28 12:54:03 -0300

    alterações

---
+++ README.md
@@ -0,0 +1,27 @@
+# Instagram Back-end
+
+### Dependencies
+- Node
+---
+### Install
+```
+$ npm i
+```
+---
+### create env file
+- create file .env
+```
+DB_USER=
+DB_PASS=
+DB_HOST=
+```
+
+--- 
+### run dev
+```
+$ npm run dev
+```
+- app is running in port 3333
+
+---
+
---
+++ src/index.js
@@ -0,0 +1,35 @@
+const express = require('express');
+const db = require('./database');
+const mongoose = require('mongoose');
+const path = require('path');
+const cors = require('cors');
+
+const app = express();
+
+const server = require('http').Server(app);
+const io = require('socket.io')(server);
+
+const main = async () => {
+    try {
+      await db();
+  
+    } catch (error) {
+      console.log(error);
+    }
+  };
+  
+main();
+
+app.use((req, res, next) => {
+    req.io = io;
+    
+    next();
+});
+ 
+app.use(cors());
+
+app.use('/files', express.static(path.resolve(__dirname, '..', 'uploads', 'resized')));
+
+app.use(require('./routes'));
+
+server.listen(3333);
